d50b27c2aca4b8742d43bc17a37b947155b04bcf
缪旭源 <1103859253@qq.com>
2021-05-11 00:46:03 +0800

    day1

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="div">
+    home
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello World"
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
